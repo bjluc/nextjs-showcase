aa6fc539cc33e87321ee42a54cd0720ffe2c221a
Luc Audemard <bjluc@bjluc.co.uk>
2023-12-26 17:40:29 +0000

    remove dynamic

--- app/Tasks/page.js
+++ app/Tasks/page.js
@@ -1,6 +1,5 @@
 import TaskFormCustom from '@/components/TaskFormCustom'
 import TaskList from '@/components/TaskList'
-export const dynamic = 'force-dynamic'
 
 const TasksPage = () => {
   return (
